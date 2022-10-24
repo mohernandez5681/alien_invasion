47333c98910d6bad5fd9e66a891fd176a23d94fe
mohernandez5681 <115736463+mohernandez5681@users.noreply.github.com>
2022-10-24 14:00:27 -0400

    24OCT2022 1400

--- blue_sky.py
+++ blue_sky.py
@@ -2,9 +2,22 @@ import pygame
 import sys
 import time
 
+class Picture():
+    def __init__(self,image_address):
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.image = pygame.image.load(image_address)
+        self.rect = self.image.get_rect()
+
+    def draw(self):
+        self.rect.center = self.screen_rect.center
+        self.screen.blit(self.image, self.rect)
+
 pygame.init()
 screen = pygame.display.set_mode((600, 400))
 screen.fill((0, 0, 255))
 
+ship = Picture(screen,"ship.bmp")
+
+ship.draw()
 pygame.display.flip()
-time.sleep(8)
