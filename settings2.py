84b2bc2195c474e23151f5104fe41ea94d257f89
mohernandez5681 <115736463+mohernandez5681@users.noreply.github.com>
2022-10-31 22:49:48 -0400

    2249
    12-5

--- settings2.py
+++ settings2.py
@@ -3,8 +3,6 @@ class Settings:
 
     def __init__(self):
         """Initialize the game's settings."""
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (255, 255, 255)
-
-        self.rocket_speed = 1.5
+        self.screen_width = 600
+        self.screen_height = 400
+        self.bg_color = (40, 160, 200)
