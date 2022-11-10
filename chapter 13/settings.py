dd2ea39bae188e003be16f571ebad86619cdf2ff
mohernandez5681 <115736463+mohernandez5681@users.noreply.github.com>
2022-11-10 18:58:11 -0500

    13-5

--- chapter 13/settings.py
+++ chapter 13/settings.py
@@ -1,5 +1,5 @@
 class Settings:
-    """A class to store all settings for the game."""
+    """A class to store all settings for Sideways Shooter."""
 
     def __init__(self):
         """Initialize the game's settings."""
@@ -8,5 +8,16 @@ class Settings:
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
 
-        # Raindrop settings
-        self.raindrop_speed = 0.5
+        # Ship settings
+        self.ship_speed = 3.0
+
+        # Bullet settings
+        self.bullet_speed = 6.0
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_frequency = 0.008
+        self.alien_speed = 0.5
